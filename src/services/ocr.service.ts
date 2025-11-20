18f0152fd6930eb449c4270fb3927962db1a6acc
Ekam <bhat0155@algonauinlive.com>
2025-11-20 16:49:32 -0500

    integrated google vision to extract text from reciept

--- src/services/ocr.service.ts
+++ src/services/ocr.service.ts
@@ -1,9 +1,24 @@
+import { ImageAnnotatorClient } from "@google-cloud/vision";
+const client = new ImageAnnotatorClient();
+
 export const ocrService = {
     // here we stimulate reading a buffer and retriving text
     async extractText(imageBuffer: Buffer): Promise<string>{
-        // in day 3, we will put google vison code here
-        // for now, return the fake text
-        console.log("Stub OCR, processing the image");
-        return "Milk 4.99\nBread 2.49\nEggs 3.99";
+       console.log("Sending the image to google vision");
+
+       // send the image buffer to google
+       const [result] = await client.documentTextDetection({
+        image: {
+            content: imageBuffer
+        }
+       })
+       // extract text from result
+       const fullText = result.fullTextAnnotation?.text;
+
+       if(!fullText){
+        throw new Error("no text found in this image");
+       }
+       console.log(`google vision found text: ${fullText}`);
+       return fullText;
     }
 }
