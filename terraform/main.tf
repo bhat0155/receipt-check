47144912f77699db60cf7838ddbadfb964a4c559
Ekam Bhatia <ekamsingh643@gmail.com>
2026-06-26 16:48:22 -0400

    fix: restore WEBSITE_RUN_FROM_PACKAGE and include prod node_modules in artifact

--- terraform/main.tf
+++ terraform/main.tf
@@ -43,8 +43,8 @@ resource "azurerm_linux_web_app" "main" {
       app_command_line = "node dist/server.js"
   }
    app_settings = {
-      "SCM_DO_BUILD_DURING_DEPLOYMENT" = "true"
-      "NODE_ENV"                       = "production"
+      "WEBSITE_RUN_FROM_PACKAGE" = "1"
+      "NODE_ENV"                 = "production"
     }
 }
 
